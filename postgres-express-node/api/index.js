6519fcb2a1facee420d0be0b62cc399a6cd5ba03
aradac <aradac00@fesb.hr>
2020-11-04 13:30:54 +0100

    Implement initial login logic

--- postgres-express-node/api/index.js
+++ postgres-express-node/api/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const hello = require("./routes/hello");
+//const { login } = require("./routes/login.schemas");
 const user = require("./routes/user");
+const login = require("./routes/login");
 
 module.exports = () => {
   hello(router);
   user(router);
+  login(router)
 
   return router;
 };
