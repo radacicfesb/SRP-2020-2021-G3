e8fe9ad69512452ae477efc91c88bedd9f00a105
aradac <aradac00@fesb.hr>
2020-12-02 13:05:41 +0100

    Refactor login to issue true JWT tokens

--- postgres-express-node/config/index.js
+++ postgres-express-node/config/index.js
@@ -26,4 +26,10 @@ module.exports = {
   api: {
     prefix: "/api",
   },
+
+  jwt: {
+    secret: process.env.JWT_SECRET, 
+    expiresIn: process.env.JWT_DURATION || "1h"
+  }
+
 };
--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
@@ -10,6 +13,42 @@ class LoginService {
     });
     return user;
   }
+  async login({username, password}) {
+    const userRecord = await this.userModel.findOne({
+      where: {username},
+    });
+
+      if (!userRecord) {
+        this.logger.error("User not registered");
+        throw new Error("Authentication failed");
+      }
+      this.logger.info("Checking password");
+      if(userRecord.password === password) {
+        this.logger.info("Password correct");
+
+        const user = {
+          username: userRecord.username,
+          role: userRecord.role || "guest"
+        };
+
+        const payload = {
+          ...user,
+          aud: config.jwt.audience || "loacalhost/api",
+          iss: config.jwt.issuer || "loacalhost@fesb",
+        };
+
+        const token = this.generateToken(payload);
+
+        return { user, token };
+      }
+  }
+
+  generateToken(payload) {
+      return jwt.sign(payload, config.jwt.secret, {expiresIn: config.jwt.expiresIn,
+      });
+  }
+
 }
+
 
 module.exports = LoginService;
